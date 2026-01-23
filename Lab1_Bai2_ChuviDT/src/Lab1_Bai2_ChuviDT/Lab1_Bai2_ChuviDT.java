3b8c52b24ba2caa97b7ace878125e7f46a466bb1
hyhung <hunghuy0925@gmail.com>
2026-01-23 20:39:18 +0700

    feat: finished Lab1_Bai2_ChuviDT

---
+++ Lab1_Bai2_ChuviDT/src/Lab1_Bai2_ChuviDT/Lab1_Bai2_ChuviDT.java
@@ -0,0 +1,25 @@
+package Lab1_Bai2_ChuviDT;
+
+import java.util.Scanner;
+
+public class Lab1_Bai2_ChuviDT {
+
+//	Viết chương trình nhập từ bàn phím 2 cạnh của hình chữ nhật. Tính và xuất chu vi, diện tích và cạnh nhỏ của hình chữ nhật.
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		System.out.print("Nhập chiều dài: ");
+		double a = s.nextDouble();
+		System.out.print("Nhập chiều rộng: ");
+		double b = s.nextDouble();
+
+		double c = 2 * (a + b);
+		double d = a * b;
+		double e = Math.min(a, b);
+
+		System.out.println("Chu vi: " + c);
+		System.out.println("Diện tích: " + d);
+		System.out.println("Cạnh nhỏ: " + e);
+
+	}
+}
