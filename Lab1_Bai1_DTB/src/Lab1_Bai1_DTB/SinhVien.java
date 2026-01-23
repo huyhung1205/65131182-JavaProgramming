e97fdb227ca9cd6a5cf91c07f31a7d578a57f410
hyhung <hunghuy0925@gmail.com>
2026-01-23 20:28:32 +0700

    feat: finished Lab1_Bai1_DTB

---
+++ Lab1_Bai1_DTB/src/Lab1_Bai1_DTB/SinhVien.java
@@ -0,0 +1,20 @@
+package Lab1_Bai1_DTB;
+
+import java.util.Scanner;
+
+public class SinhVien {
+
+//	Viết chương trình cho phép nhập họ và tên sinh viên, điểm trung bình từ bàn phím sau đó xuất ra màn hình.
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		System.out.print("Nhập họ và tên sinh viên: ");
+		String fullName = s.nextLine();
+
+		System.out.print("Nhập điểm trung bình: ");
+		double score = s.nextDouble();
+
+		System.out.print(fullName + ": " + score + " điểm.");
+	}
+
+}
