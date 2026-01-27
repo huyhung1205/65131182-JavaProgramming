45b01899eebe22dfcba49eba19031afe8a1beb2f
hyhung <hunghuy0925@gmail.com>
2026-01-27 15:55:29 +0700

    feat: finished Lab1Bai1

---
+++ Lab1/src/Lab1Bai1.java
@@ -0,0 +1,20 @@
+
+// Viết chương trình cho phép nhập họ và tên sinh viên, điểm trung bình từ bàn phím.
+// Xuất ra màn hình với định dạng: <<họ và tên>><<điểm>>điểm.
+
+import java.util.Scanner;
+
+public class Lab1Bai1 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Nhập họ và tên: ");
+		String name = s.nextLine();
+
+		System.out.print("Nhập điểm trung bình: ");
+		double scrore = s.nextDouble();
+
+		System.out.printf("%s %.2f điểm.", name, scrore);
+
+		s.close();
+	}
+}
