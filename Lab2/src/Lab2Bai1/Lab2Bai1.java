51f8dcbed306e77172ef5dedced4974af42a1119
hyhung <hunghuy0925@gmail.com>
2026-02-24 23:02:39 +0700

    feat: finished Lab2Bai1

---
+++ Lab2/src/Lab2Bai1/Lab2Bai1.java
@@ -0,0 +1,26 @@
+package Lab2Bai1;
+
+// Viết chương trình cho phép giải phương trình bậc nhất
+// trong đó các hệ số a và b nhập từ bàn phím
+
+import java.util.Scanner;
+
+public class Lab2Bai1 {
+    static void main() {
+        System.out.println("Chương trình: Giải phương trình bậc nhất: ax + b = 0");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Nhập hệ số a: ");
+        double a = scan.nextDouble();
+        System.out.print("Nhập hệ số b: ");
+        double b = scan.nextDouble();
+
+        if (a == 0) {
+            if (b == 0)
+                System.out.println("Phương trình có vô số nghiệm");
+            else
+                System.out.println("Phương trình vô nghiệm");
+        } else {
+            System.out.print("Nghiệm của phương trình: " + (-b/a));
+        }
+    }
+}
